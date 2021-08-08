5eed8a1267851e41b6b365619515df849348761c
ekhalturin-ki15 <1tigrin@mail.ru>
2021-08-08 15:30:09 +0700

    3

--- new_program.cpp
+++ new_program.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 // #include "bits/stdc++.h
+
+
+
 
 
 using namespace std;
@@ -15,36 +19,21 @@ int main()
 	ifstream in("input.txt");
 	ofstream out("output.txt");
 
+
+	std::vector<int> a = { 0, 4, 6, 50 };
+	std::vector<int> b = { -23, 4, 4, 5, 6, 50, 60 };
+
+
+	vector<int> c;
+
+
+	merge(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(c)); // back_inserter
+
+
 	//int char bool long long unsigned float double long double 
 
-
-	// -100 100
-	int n, a;
-	in >> n;
-
-	const int min_val = -100;
-	const int max_val = 100;
-	vector<int> v(max_val - (min_val)+1);
-
-
-	for (int i = 0; i < n; ++i)
-	{
-		in >> a;
-		++v[a - min_val];
-	}
-
-	for (int i = 0; i < v.size(); ++i)
-	{
-		for (int j = 0; j < v[i]; ++j)
-		{
-			out << i + min_val << " ";
-		}
-
-	}
-
-
-
-
+	// 1 3 7 8 9 7 3 3 7 8 9 2 7 
+	// 8 
 
 
 
