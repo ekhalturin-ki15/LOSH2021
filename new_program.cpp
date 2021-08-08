384bd02b6b8835b1bccd7a9dcbf5c070855de644
ekhalturin-ki15 <1tigrin@mail.ru>
2021-08-08 15:30:23 +0700

    4

--- new_program.cpp
+++ new_program.cpp
@@ -2,16 +2,28 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 // #include "bits/stdc++.h
-
-
-
 
 
 using namespace std;
 
-int main()
+
+//int main(int argn, vector<vector<char>> argv)
+int main(int argn, char** argv)
 {
+
+
+
+	//argn = 6;
+	// 
+	// 
+	// argv[2][1] == '5'
+	// "prog.exe"
+	// "-g"
+	// "-5"
+	// 
+	// 
 	//freopen_s("input.txt", "r", stdin);
 	//freopen("output.txt", "w", stdout);
 
@@ -22,19 +34,6 @@ int main()
 
 	std::vector<int> a = { 0, 4, 6, 50 };
 	std::vector<int> b = { -23, 4, 4, 5, 6, 50, 60 };
-
-
-	vector<int> c;
-
-
-	merge(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(c)); // back_inserter
-
-
-	//int char bool long long unsigned float double long double 
-
-	// 1 3 7 8 9 7 3 3 7 8 9 2 7 
-	// 8 
-
 
 
 
