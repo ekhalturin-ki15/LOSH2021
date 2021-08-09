29e19200b11649beaaf3d6036573c69dc2edc694
ekhalturin-ki15 <1tigrin@mail.ru>
2021-08-09 14:04:51 +0700

    10 05

--- new_program.cpp
+++ new_program.cpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <cstdio>
 #include <functional>
+#include <list>
 
 #pragma comment(linker, "/STACK:26777216")
 // #include "bits/stdc++.h
@@ -12,16 +13,56 @@
 using namespace std;
 
 
+struct A
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+
+
+
+	int size()
+	{
+		return a + b + c;
+	}
+
+};
+
+struct B
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+
+
+
+	string size()
+	{
+		return "string";
+	}
+
+};
+
+
+
+auto func(auto a)
+{
+	return a.size();
+}
 
 
 
 int main(int argn, char** argv)
 {
+
+#ifdef TEST
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
+#endif
 
 
 
+	
 }
 
 
